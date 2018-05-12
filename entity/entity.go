942a6749ba4ff811fb445f51f9a08302da382762
darbs <darby.perez@gmail.com>
2018-05-11 22:16:33 -0500

    add geo coords

--- entity/entity.go
+++ entity/entity.go
@@ -2,17 +2,41 @@ package entity
 
 import (
 	"encoding/json"
+	"errors"
 )
 
+/*
+Entity struct
+ */
 type Entity struct {
-	Location string `json:"location"`
+	Altitude float32 `json:"altitude"`
+	Longitude float32 `json:"longitude"`
+	Latitude float32 `json:"latitude"`
 	Health float32 	`json:"health"`
 	Mobile bool		`json:"mobile"`
 }
 
-// todo handle empty objects or missing attributes
+/*
+Validate an entities structure
+ */
+func (e Entity) valid () (error) {
+	if (e.Health < 0) {
+		return errors.New("Health must be greater than zero")
+	}
+
+	return nil
+}
+
+/*
+Create new Entity from json string
+ */
 func FromJson(jsonStr string) (Entity, error){
 	var entity Entity
 	err := json.Unmarshal([]byte(jsonStr), &entity)
+	if (err != nil) {
+		return entity, err
+	}
+
+	err = entity.valid()
 	return entity, err
 }
--- test/entity_test.go
+++ test/entity_test.go
@@ -2,12 +2,29 @@ package test
 
 import (
 	"testing"
+	"fmt"
 	"github.com/darbs/atlas/entity"
 )
 
-func TestEntityParser (t *testing.T){
+func TestEntityParserEmpty (t *testing.T){
 	_, err := entity.FromJson("{}")
 	if (err != nil) {
 		t.Errorf("Failed to parse empty Object")
+	}
+}
+
+func TestEntityParserValid (t *testing.T){
+	entity, err := entity.FromJson("{\"latitude\": 30.307182, \"longitude\": -97.755996, \"altitude\": 489}")
+	if (err != nil) {
+		fmt.Printf("Resulting entity: %v error: %v", entity, err)
+		t.Errorf("Failed to parse valid Object")
+	}
+}
+
+func TestEntityParserInvalid (t *testing.T){
+	entity, err := entity.FromJson("{\"health\": -1}")
+	if (err == nil) {
+		fmt.Printf("Resulting entity: %v error: %v", entity, err)
+		t.Errorf("Failed to catch negative health")
 	}
 }
